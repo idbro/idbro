14e31285d38f9bbc342e67fa463c8de76b443e97
Kaka <drkaka@drkaka.com>
2018-11-24 16:23:54 +0800

    Add get/set methods for storage layer. Add bbolt implementation and test cases.

--- storage/storage.go
+++ storage/storage.go
@@ -1,10 +1,34 @@
 package storage
 
+import "errors"
+
+type (
+	// Record is a piece of information with ID and Meta data.
+	Record struct {
+		ID   []byte
+		Meta map[string]interface{}
+	}
+)
+
+var (
+	// ErrRecordExists is returned when creating a record that already exists.
+	ErrRecordExists = errors.New("record already exists")
+
+	// ErrRecordNotExists is returned when trying to access a record which not exists.
+	ErrRecordNotExists = errors.New("record not exists")
+)
+
 // Storage interface is used to define behaviors for storage.
 type Storage interface {
-	// Initialize the storage
+	// Initialize the storage. Used for creating the db file, ping db server and related initialization jobs.
 	Initialize() error
 
-	// Close to clean up the database
+	// Close to clean up the database, connections and so on.
 	Close() error
+
+	// Insert a new record with given meta data.
+	Insert(id []byte, meta map[string]interface{}) error
+
+	// Get a record with its ID
+	Get(id []byte) (*Record, error)
 }
